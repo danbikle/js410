aff63a819a6236f59a8008cf83af8cbd506592c5
Dan Bikle <danbikle@outlook.com>
2018-01-30 16:25:39 -0800

    hello

---
+++ README.md
@@ -0,0 +1,15 @@
+# README.md
+
+This repo is intended to run on Heroku.
+
+I offer you some shell commands which might be helpful:
+
+```bash
+~/heroku/bin/heroku auth:login
+mkdir ~/js/
+cd    ~/js/
+git clone https://github.com/danbikle/js410
+cd js410
+~/heroku/bin/heroku create js410yourname
+git push heroku master
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+~/js/js410/index.js
+
+This script should help you learn JavaScript.
+*/
+
+var abc = 123.4
+var def = "hello"
+var ghi = [abc, def]
+var jkl = {def: abc, abc: def}
+
+function f10(x) {
+    return x + 1
+}
+
+f11 = function(x) {
+    return x + 2
+}
+
+function prnt(x) {
+    console.log(x)
+    return true
+}
+
+prnt(f11(abc))
+
+
+
