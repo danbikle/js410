f5bb2d9a53f0f7761051921404ba780901afabd0
Dan Bikle <danbikle@outlook.com>
2018-02-01 15:49:15 -0800

    more

--- index33.js
+++ index33.js
@@ -8,9 +8,37 @@ This script should help you learn JavaScript.
 var num_i = 7
 
 // I should create a number [ decimal actually ]
-var float_i = 1.2345
+var float_i = 1.2345 // AKA: "Float"
 
-// I should create a string
+// I should create strings
 var some1_s = 'I can use single quotes'
 var some2_s = "I can use double quotes"
+var some3_s = "I can use 'both' quotes"
+var some4_s = 'You can use "both" quotes'
+
+// I should create an array
+var some_a = [num_i, float_i, some1_s, ['arrayinarray',411]]
+
+// I should create an object
+var some_o = {'key0': 'hello', num_i: 'world'}
+
+// I should create two booleans
+var is_true  = true
+var is_false = false
+
+// I should create a function
+function f10(x) {
+    return x + x
+}
+
+// I should create a function this way:
+var f11 = function(x) {
+    return x + 4
+}
+
+// I like this function too:
+print = function(x) {
+    console.log(x)
+    return true
+}
 
