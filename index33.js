1887d0b54762cf4e7bbbb88ac258d4e9ba27c44b
Dan Bikle <danbikle@outlook.com>
2018-02-01 13:22:02 -0800

    more

---
+++ index33.js
@@ -0,0 +1,16 @@
+/*
+~/js/js410/index32.js
+
+This script should help you learn JavaScript.
+*/
+
+// I should create a number [ integer actually ]
+var num_i = 7
+
+// I should create a number [ decimal actually ]
+var float_i = 1.2345
+
+// I should create a string
+var some1_s = 'I can use single quotes'
+var some2_s = "I can use double quotes"
+
