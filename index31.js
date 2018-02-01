3f114a52b8336e9c0fe263ec5975cbda35a2a7cc
Dan Bikle <danbikle@outlook.com>
2018-01-31 23:36:36 -0800

    more

---
+++ index31.js
@@ -0,0 +1,10 @@
+/*
+~/js/js410/index31.js
+
+This script should help you learn JavaScript.
+*/
+
+var x31 = 31.2
+
+
+
---
+++ index32.js
@@ -0,0 +1,10 @@
+/*
+~/js/js410/index32.js
+
+This script should help you learn JavaScript.
+*/
+
+var x32 = 32.3
+
+
+
